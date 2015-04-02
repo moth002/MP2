47c6cb05162be1eed8d584264c18dd3b52696df5
moth002 <moth002@gmail.com>
2015-04-02 15:43:27 +1300

    Trying Insomnia plugin

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
         function ($ionicPlatform, globalIdService, $injector, cordovaReadyService, $ionicPopup) {
             $ionicPlatform.ready(function () {
 
+                cordovaReadyService(window.plugins.insomnia.keepAwake());
+
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
             if (window.cordova && window.cordova.plugins.Keyboard) {

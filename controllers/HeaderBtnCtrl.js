9a5e8ca2f10b34fe7e369f487730cdcf803d4e3d
moth002 <moth002@gmail.com>
2015-04-13 11:58:57 +1200

    log off prompt with home button

--- controllers/HeaderBtnCtrl.js
+++ controllers/HeaderBtnCtrl.js
@@ -1,14 +1,29 @@
 ﻿angular.module('myApp')
     .controller("HeaderBtnCtrl", [
-        '$scope', '$rootScope', '$ionicSideMenuDelegate',
-            function ($scope, $rootScope, $ionicSideMenuDelegate) {
+        '$scope', '$rootScope', '$ionicSideMenuDelegate', '$ionicPopup',
+            function ($scope, $rootScope, $ionicSideMenuDelegate, $ionicPopup) {
 
                 $scope.btnSideMenu = function () {
                     $ionicSideMenuDelegate.toggleLeft();
                 };
 
                 $scope.btnGoHome = function () {
-                    window.location = '#/';
+                    $ionicPopup.confirm({
+                        title: 'Log off',
+                        template: 'Are you sure you want to Log Off?'
+                    }).then(function (res) {
+                        if (res) {
+                            var nav = window.navigator;
+                            if (this.phonegapNavigationEnabled &&
+                                nav &&
+                                nav.app &&
+                                nav.app.backHistory) {
+                                nav.app.backHistory();
+                            } else {
+                                window.history.back();
+                            }
+                        }
+                    });
                 };
             }
     ]);

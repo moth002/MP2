582ba94099ba06fd3b0902ce4f0ba930187bac29
moth002 <moth002@gmail.com>
2015-04-13 12:19:05 +1200

    Fixed log off feature

--- controllers/UserCtrl.js
+++ controllers/UserCtrl.js
@@ -19,15 +19,7 @@
                         template: 'Are you sure you want to Log Off?'
                     }).then(function (res) {
                         if (res) {
-                            var nav = window.navigator;
-                            if( this.phonegapNavigationEnabled &&
-                                nav &&
-                                nav.app &&
-                                nav.app.backHistory ){
-                                nav.app.backHistory();
-                            } else {
-                                window.history.back();
-                            }
+                            window.location = '#/';
                         }
                     });
                 }

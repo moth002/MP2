42fc3a58577c59f1950700e9f48ef39a535539c5
moth002 <moth002@gmail.com>
2015-04-28 15:30:10 +1200

    New logo files

---
+++ services/EditMainListService.js
@@ -0,0 +1,15 @@
+﻿angular.module('mobilePhlebotomy')
+    .factory('editMainListService', function () {
+        var editMainList = {
+            editAllowed: false,
+
+            getEditAllowed: function () {
+                return editMainList.editAllowed;
+            },
+            setEditAllowed: function () {
+                editMainList.editAllowed = !editMainList.editAllowed;
+            }
+        };
+
+        return editMainList;
+});
